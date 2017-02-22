e403b89ce11336dd2b288d0bff0341b5246d5493
Piao Piao <94313353@qq.com>
2017-02-22 14:35:14 +0800

    modify

---
+++ InputExample/MessageTableView.h
@@ -0,0 +1,13 @@
+//
+//  MessageTableView.h
+//  InputExample
+//
+//  Created by Piao Piao on 2017/2/22.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTableView : UIView
+
+@end
---
+++ InputExample/MessageTableView.m
@@ -0,0 +1,21 @@
+//
+//  MessageTableView.m
+//  InputExample
+//
+//  Created by Piao Piao on 2017/2/22.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import "MessageTableView.h"
+
+@implementation MessageTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
